951cc19fccacde55b6b41cd3574f41c67b64652d
iatraoui <iatraoui@argans.eu>
2023-09-07 13:43:21 +0200

    formatting with black

--- src/training_raster_clipper/implementation/iatraoui.py
+++ src/training_raster_clipper/implementation/iatraoui.py
@@ -4,6 +4,7 @@ import xarray as xr
 from geopandas.geodataframe import GeoDataFrame
 from geopandas import read_file
 from rioxarray import open_rasterio
+from rasterio.features import rasterize
 import numpy as np
 
 xr.set_options(keep_attrs=True)
@@ -42,23 +43,25 @@ def load_sentinel_data(
         xr.DataArray: A DataArray containing the 3 RGB bands from the visible spectrum
     """
 
-    # ...  # TODO
-    # raise NotImplementedError
-    # p = Path(sentinel_product_location)
-    # paths_list = [x for x in p.iterdir() if x.is_file()]
-    # colors_list = [str(x).split('_')[-2] for x in paths_list]
-    # data_dict = dict(zip(colors_list, paths_list))
-    # data_arrays_list = 
     paths_list = {
-        band_name : list(sentinel_product_location.glob(f'GRANULE/*/IMG_DATA/R{resolution}m/*_{band_name}_*'))[0]
+        band_name: list(
+            sentinel_product_location.glob(
+                f"GRANULE/*/IMG_DATA/R{resolution}m/*_{band_name}_*"
+            )
+        )[0]
         for band_name in band_names
     }
-    data_arrays_list = [open_rasterio(paths_list[band_name]).assign_coords({'band' : [band_name]}) for band_name in band_names]
-    rasters_data_array = xr.concat(data_arrays_list, dim = 'band')
-    rasters_data_array = rasters_data_array.where(rasters_data_array != 0, np.float32(np.nan))
-    RADIO_ADD_OFFSET = -1000
-    QUANTIFICATION_VALUE = 10000
-    result = ((rasters_data_array + RADIO_ADD_OFFSET) / QUANTIFICATION_VALUE)
+    data_arrays_list = [
+        open_rasterio(paths_list[band_name]).assign_coords({"band": [band_name]})
+        for band_name in band_names
+    ]
+    rasters_data_array = xr.concat(data_arrays_list, dim="band")
+    rasters_data_array = rasters_data_array.where(
+        rasters_data_array != 0, np.float32(np.nan)
+    )
+    radio_add_offset = -1000
+    quantification_value = 10000
+    result = (rasters_data_array + radio_add_offset) / quantification_value
     return result
 
 
@@ -79,8 +82,22 @@ def rasterize_geojson(
                       0 where no polygon were found, and integers representing classes in order of occurence in the GeoDataFrame
     """
 
-    ...  # TODO
-    raise NotImplementedError
+    # ...  # TODO
+    # raise NotImplementedError
+
+    _, x_shape, y_shape = data_array.shape
+    shapes = list(training_classes.get("geometry"))
+    print("shapes = ", shapes)
+    out_shape = (x_shape, y_shape)
+    print("out_shape = ", out_shape)
+    transform = data_array.spatial_ref.GeoTransform
+    transform = training_classes.affine_transform([float(x) for x in transform.split()])
+    print("transform = ", transform)
+
+    burnt_polygons = rasterize(shapes, out_shape, transform)
+    print(training_classes.get("class"))
+    mapping = {}
+    return (burnt_polygons, mapping)
 
 
 def produce_clips(
