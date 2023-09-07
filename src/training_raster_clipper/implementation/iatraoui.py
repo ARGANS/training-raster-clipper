77900797a802f533b1d1da8f620ba572f1d5d5a0
iatraoui <144032377+iatraoui@users.noreply.github.com>
2023-09-07 13:51:11 +0200

    update partie 2 pull request (#11)
    * update partie 2
    
    * Update iatraoui.py
    
    * formatting with black

--- src/training_raster_clipper/implementation/iatraoui.py
+++ src/training_raster_clipper/implementation/iatraoui.py
@@ -3,6 +3,11 @@ from pathlib import Path
 import xarray as xr
 from geopandas.geodataframe import GeoDataFrame
 from geopandas import read_file
+from rioxarray import open_rasterio
+from rasterio.features import rasterize
+import numpy as np
+
+xr.set_options(keep_attrs=True)
 
 from training_raster_clipper.custom_types import (
     BandNameType,
@@ -38,8 +43,26 @@ def load_sentinel_data(
         xr.DataArray: A DataArray containing the 3 RGB bands from the visible spectrum
     """
 
-    ...  # TODO
-    raise NotImplementedError
+    paths_list = {
+        band_name: list(
+            sentinel_product_location.glob(
+                f"GRANULE/*/IMG_DATA/R{resolution}m/*_{band_name}_*"
+            )
+        )[0]
+        for band_name in band_names
+    }
+    data_arrays_list = [
+        open_rasterio(paths_list[band_name]).assign_coords({"band": [band_name]})
+        for band_name in band_names
+    ]
+    rasters_data_array = xr.concat(data_arrays_list, dim="band")
+    rasters_data_array = rasters_data_array.where(
+        rasters_data_array != 0, np.float32(np.nan)
+    )
+    radio_add_offset = -1000
+    quantification_value = 10000
+    result = (rasters_data_array + radio_add_offset) / quantification_value
+    return result
 
 
 def rasterize_geojson(
@@ -59,8 +82,22 @@ def rasterize_geojson(
                       0 where no polygon were found, and integers representing classes in order of occurence in the GeoDataFrame
     """
 
-    ...  # TODO
-    raise NotImplementedError
+    # ...  # TODO
+    # raise NotImplementedError
+
+    _, x_shape, y_shape = data_array.shape
+    shapes = list(training_classes.get("geometry"))
+    print("shapes = ", shapes)
+    out_shape = (x_shape, y_shape)
+    print("out_shape = ", out_shape)
+    transform = data_array.spatial_ref.GeoTransform
+    transform = training_classes.affine_transform([float(x) for x in transform.split()])
+    print("transform = ", transform)
+
+    burnt_polygons = rasterize(shapes, out_shape, transform)
+    print(training_classes.get("class"))
+    mapping = {}
+    return (burnt_polygons, mapping)
 
 
 def produce_clips(
